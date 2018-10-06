5eeeb9f2beff87b6b5740afcc5bf1c7858a5890e
ssab <qasw4554@gmail.com>
2018-10-06 14:28:56 +0900

    anyway until here

--- main.go
+++ main.go
@@ -5,8 +5,14 @@ import "net/http"
 func main() {
 	mux := http.NewServeMux()
 
+	files := http.FileServer(http.Dir("template/"))
+	mux.Handle("/template/", http.StripPrefix("/template", files))
+
+	mux.HandleFunc("/", index)
+
 	server := http.Server{
-		Addr:	"127.0.0.1:8080",
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
 	}
 	mux.ListenAndServe()
 }
