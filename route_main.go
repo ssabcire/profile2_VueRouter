8bf7dcfba9624bc9eeee388e4d47e3161d401595
ssab <qasw4554@gmail.com>
2018-10-06 17:23:57 +0900

    indexを改良

--- route_main.go
+++ route_main.go
@@ -1,15 +1,13 @@
 package main
 
-import (
-	"html/template"
-	"log"
-	"net/http"
-)
+import "net/http"
 
 func index(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.ParseFiles("template/MyIntroduction.html"))
-	err := t.ExecuteTemplate(w, "MyIntroduction.html", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	generateHTML(w, "layout", "header", "index")
+
+	//t := template.Must(template.ParseFiles("template/MyIntroduction.html"))
+	//err := t.ExecuteTemplate(w, "MyIntroduction.html", nil)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 }
