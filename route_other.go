67fbda83a6f5bfa933345c38ef69bd03b8ff4686
ssab <qasw4554@gmail.com>
2018-10-06 17:20:54 +0900

    generateHTMLを追加

--- route_other.go
+++ route_other.go
@@ -3,13 +3,13 @@ package main
 import "net/http"
 
 func skill(w http.ResponseWriter, r *http.Request) {
-
+	generateHTML(w, "layout", "header", "skill")
 }
 
 func sns(w http.ResponseWriter, r *http.Request) {
-
+	generateHTML(w, "layout", "header", "sns")
 }
 
 func usingTech(w htto.ResponseWriter, r *http.Request) {
-
+	generateHTML(w, "layout", "header", "usingTech")
 }
